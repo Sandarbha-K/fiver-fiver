0208222c05138be35588585be41a422f5763d3ac
Sandarbha  Komujwar <sandarbhakomujwar100@gmail.com>
2023-07-07 19:42:58 +0530

    push

--- fiverr/next.config.js
+++ fiverr/next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-    SERVER_URL:"http://localhost:3001",
-  },
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3001",
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname:"**",
       },
     ],
   },
